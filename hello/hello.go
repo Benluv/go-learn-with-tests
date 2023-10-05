3779b31fe133ad7b3d62435fec263916495efab0
Benluv <luongov13@gmail.com>
2023-10-05 17:42:43 +0200

    added additional tests for french and german support and changes to the hello.go file with some refactor

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,33 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const german = "German"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const germanHelloPrefix = "Hallo, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case german:
+		prefix = germanHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
