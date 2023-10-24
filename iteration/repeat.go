42384b1df8cc77403946fdcce6ef856eb031faac
Benluv <luongov13@gmail.com>
2023-10-24 17:03:53 +0200

    refactored the Repeat and CountRepeat function

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -3,20 +3,15 @@ package iteration
 import "strings"
 
 func Repeat(character string, repeat int) string {
-	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated += character
-	}
-	return repeated
+	return strings.Repeat(character, repeat)
 }
 
-func CountRepeat(character string, count int) int {
+func CountRepeat(character string, count int) (counted int) {
 	var repeated string
-	var counted int
 	if count == 0 {
 		count = 8
 	}
 	repeated = strings.Repeat(character, count)
 	counted = strings.Count(repeated, character)
-	return counted
+	return
 }
