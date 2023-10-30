59c115cdafb1d0fb51fa0edc527e45510c0ae806
Benluv <luongov13@gmail.com>
2023-10-30 12:16:47 +0100

    initialized tests to calculate permiter, wrote minimal code to have test run and check failing output, and then wrote enough code to make it pass

---
+++ structs-methods-interfaces/shapes_test.go
@@ -0,0 +1,12 @@
+package structsmethodsinterfaces
+
+import "testing"
+
+func TestPermiter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
