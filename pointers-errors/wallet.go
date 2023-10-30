a03f02afa94b459f95943afaa99d95485c267cf0
Benluv <luongov13@gmail.com>
2023-10-30 13:39:48 +0100

    created new type Bitcoin based off int and implemented Stringer to use %s identifier for tests

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -2,17 +2,28 @@ package pointerserrors
 
 import "fmt"
 
+type Bitcoin int
+
 // in Go if a symbol (variables, types, functions et al) starts with a lowercase then it is private outside the package it is defined in
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Stringer interface {
+	String() string
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	// if pointer removed from wallet, then the memory location of the wallet changes
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
 // no need to change Balance to use a pointer receiver. But it is convention for sake of consistency
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
