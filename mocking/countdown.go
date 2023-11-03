5eaea1525684b0579cdcc4c5923bf58634b2577d
Benluv <luongov13@gmail.com>
2023-11-03 15:21:11 +0100

    Squashed commit of the following:
    commit 388669d9ae8c36e84a2ba511002b95b3bc94dc98
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 15:19:25 2023 +0100
    
        a little bit more changes in order to match the guide of learn go with tests
    
    commit 6f9e5ceae89cbf7190d7134cc8a5f464d8680f40
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 15:16:44 2023 +0100
    
        made changes to mocking in order to match the learn-go-with-tests guide and understand a little bit more of what was explained on mocking, structs, interfaces, and methods
    
    commit 21a1c5e8e126172d13df250ffac37c05c65ea1c6
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 14:41:07 2023 +0100
    
        added the ConfigurableSleeper and deleted the DefaultSleeper
    
    commit 55d41a5cbfd15f810c41b2f1bb389d1cf613f4a2
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 14:31:27 2023 +0100
    
        refactoring tests in order to remove the sleeper check because it is already being tested for in another test
    
    commit 4468f86c6ee48ffe7a15a2ad07b4bd3462228ff4
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 14:04:52 2023 +0100
    
        made SpyCountdownOperations struct to implement both io.Writer and Sleeper to record every call
    
    commit e60d66fe0284c4432d3f5e1a95e97273abc36c90
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 13:39:14 2023 +0100
    
        made mock for tests with a sleep interface which is both used by the defaultsleeper and the one that registers the calls for the tests to check
    
    commit 3d055bce4fce6b9bd77167e627e9810af4722848
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 13:28:52 2023 +0100
    
        refactored countdown and final word into constants and also added the sleep timer, but it also slows tests
    
    commit 7856e3e9803ad516447151c4498fe8cd6f32fd0e
    Author: Benluv <luongov13@gmail.com>
    Date:   Fri Nov 3 13:26:23 2023 +0100
    
        made test for countdown from 3, 2, 1, Go! as well as the functionality
    
    commit f27a74f3898a726f1e95a28eb8525f6ffe35ea5b
    Author: Benluv <luongov13@gmail.com>
    Date:   Thu Nov 2 15:34:57 2023 +0100
    
        making changes so that it can be outputted to terminal. 'Take a thin slice of functionality and make it work end-to-end, backed by tests.' Better than doing some hacking because that causes devs to fall into rabbit holes
    
    commit 5484830af965082ae475f7f0c394b000ef27b2d0
    Author: Benluv <luongov13@gmail.com>
    Date:   Thu Nov 2 15:32:09 2023 +0100
    
        starting setting up the iterative, test-driven approach and that is why we first print 3

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,11 +7,6 @@ import (
 	"time"
 )
 
-const (
-	finalWord      = "Go!"
-	countdownStart = 3
-)
-
 type Sleeper interface {
 	Sleep()
 }
@@ -21,49 +16,18 @@ type ConfigurableSleeper struct {
 	sleep    func(time.Duration)
 }
 
-type SpyTime struct {
-	durationSlept time.Duration
-}
-
-func (s *SpyTime) Sleep(duration time.Duration) {
-	s.durationSlept = duration
-}
-
 func (c *ConfigurableSleeper) Sleep() {
 	c.sleep(c.duration)
 }
 
-type SpySleeper struct {
-	Calls int
-}
-
-type SpyCountdownOperations struct {
-	Calls []string
-}
-
-func (s *SpyCountdownOperations) Sleep() {
-	s.Calls = append(s.Calls, sleep)
-}
-
-func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
-}
-
-const write = "write"
-const sleep = "sleep"
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
+const finalWord = "Go!"
+const countdownStart = 3
 
 func Countdown(out io.Writer, sleeper Sleeper) {
-
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
-
 	fmt.Fprint(out, finalWord)
 }
 
