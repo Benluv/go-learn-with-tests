6f9e5ceae89cbf7190d7134cc8a5f464d8680f40
Benluv <luongov13@gmail.com>
2023-11-03 15:16:44 +0100

    made changes to mocking in order to match the learn-go-with-tests guide and understand a little bit more of what was explained on mocking, structs, interfaces, and methods

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -8,11 +8,10 @@ import (
 )
 
 func TestCountdown(t *testing.T) {
-	t.Run("prints 3 to Go!", func(t *testing.T) {
+	t.Run("count 1, 2, 3, Go!", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		spySleeper := &SpySleeper{}
 
-		Countdown(buffer, spySleeper)
+		Countdown(buffer, &SpyCountdownOperations{})
 
 		got := buffer.String()
 		want := `3
@@ -55,4 +54,28 @@ func TestConfigurableSleeper(t *testing.T) {
 	if spyTime.durationSlept != sleepTime {
 		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime)
 	}
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
