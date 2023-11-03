21a1c5e8e126172d13df250ffac37c05c65ea1c6
Benluv <luongov13@gmail.com>
2023-11-03 14:41:07 +0100

    added the ConfigurableSleeper and deleted the DefaultSleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -16,6 +16,23 @@ type Sleeper interface {
 	Sleep()
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 type SpySleeper struct {
 	Calls int
 }
@@ -36,12 +53,6 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 const write = "write"
 const sleep = "sleep"
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
@@ -57,6 +68,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
