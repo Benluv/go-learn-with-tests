4468f86c6ee48ffe7a15a2ad07b4bd3462228ff4
Benluv <luongov13@gmail.com>
2023-11-03 14:04:52 +0100

    made SpyCountdownOperations struct to implement both io.Writer and Sleeper to record every call

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -16,13 +16,38 @@ type Sleeper interface {
 	Sleep()
 }
 
+type SpySleeper struct {
+	Calls int
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
+
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
